0dcedcd2158a9b5c291188f8a7c361c9853f3068
Md Salim Shahed Shajid <mdsalimshahed@gmail.com>
2026-06-09 13:16:13 +0600

    revamp

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
   },
   build: {
     // Prevent aggressive CSS minification that might remove important rules
-    minify: 'terser',
     cssMinify: 'lightningcss'
   }
 })
